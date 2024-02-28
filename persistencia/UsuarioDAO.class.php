a103a41b189f414892da1a8f8eb8f1b4731ad32b
Luis Felipe Maior <luis.maior@canoastec.rs.gov.br>
2024-02-28 10:22:46 -0300

    Modelo inicial retornando buscas do filtro

--- persistencia/UsuarioDAO.class.php
+++ persistencia/UsuarioDAO.class.php
@@ -3,19 +3,22 @@
 include_once DIR_PERSISTENCIA . 'Conexao.class.php';
 include_once DIR_MODELO . 'UsuarioVO.class.php';
 
-class UsuarioDAO {
-    
+class UsuarioDAO
+{
+
     private $conexao = null;
-    
-    function __construct() {
+
+    function __construct()
+    {
     }
-    
-    function listar($filtros = '') {
-        try{
+
+    function listar($filtros = '')
+    {
+        try {
             $query = ("SELECT * FROM usuario u WHERE 1=1 $filtros ORDER BY u.nm_usuario ASC");
             $this->conexao = Conexao::connect()->query($query);
             $array = $this->conexao->fetchAll(PDO::FETCH_ASSOC);
-            
+
             $usuarios = array();
 
             foreach ($array as $obj) {
@@ -36,16 +39,17 @@ class UsuarioDAO {
             }
             $this->conexao = null;
             return $usuarios;
-        }catch(Exception $e){
+        } catch (Exception $e) {
             echo "Erro ao buscar os Usuarios";
             return false;
         }
     }
 
- 
 
-    function cadastrar(UsuarioVO $usuario){
-        try{
+
+    function cadastrar(UsuarioVO $usuario)
+    {
+        try {
 
             $sql = "
                 INSERT INTO usuario(
@@ -79,12 +83,60 @@ class UsuarioDAO {
             $this->conexao->execute();
             $this->conexao = null;
             return true;
-
-        }catch(Exception $e){
+        } catch (Exception $e) {
             echo "Erro ao cadastrar o Usuario";
             return false;
         }
     }
 
-
+    // function filtrar($filtro)
+    // {
+    //     switch ($filtro) {
+    //         case (isset($filtro['cpf']) && isset($filtro['name'])): {
+    //                 try {
+    //                     $cpf = $filtro['cpf'];
+    //                     $name = $filtro['name'];
+    //                     $query = ("SELECT * FROM usuario user WHERE 1=1 AND user.nm_usuario like '%$name%' AND user.nr_cpf like '%$cpf%' ORDER BY user.nm_usuario ASC");
+    //                     $this->conexao = Conexao::connect()->query($query);
+    //                     $this->conexao->execute();
+    //                     $array = $this->conexao->fetchAll(PDO::FETCH_ASSOC);
+    //                     foreach ($array as $obj) {
+    //                         $usuario = new UsuarioVO();
+    //                         $usuario->setIdUsuario($obj['id_usuario']);
+    //                         $usuario->setNmUsuario($obj['nm_usuario']);
+    //                         $usuario->setNrCpf($obj['nr_cpf']);
+    //                         $usuario->setDsEmail($obj['ds_email']);
+    //                         $usuario->setDsLogin($obj['ds_login']);
+    //                         $usuario->setPwSenha($obj['pw_senha']);
+    //                         $usuario->setIdPerfil($obj['id_perfil']);
+    //                         $usuario->setAoStatus($obj['ao_status']);
+    //                         $usuario->setIdUsuarioinclusao($obj['id_usuarioinclusao']);
+    //                         $usuario->setIdUsuarioalteracao($obj['id_usuarioalteracao']);
+    //                         $usuario->setDtCadastro($obj['dt_cadastro']);
+    //                         $usuario->setDtAlteracao($obj['dt_alteracao']);
+    //                         $usuarios[] = $usuario;
+    //                     }
+    //                     $this->conexao = null;
+    //                     return $usuarios;
+    //                 } catch (Exception $e) {
+    //                     echo "Erro ao cadastrar o Usuario";
+    //                     return false;
+    //                 }
+    //             }
+    //         case (isset($filtro['name'])): {
+    //                 $name = $filtro['name'];
+    //                 $query = ("SELECT * FROM usuario user WHERE 1=1 AND user.nm_usuario like '%$name%' ORDER BY user.nm_usuario ASC");
+    //                 $this->conexao = Conexao::connect()->query($query);
+    //                 $this->conexao->execute();
+    //                 $this->conexao = null;
+    //             }
+    //         case (isset($filtro['cpf'])): {
+    //                 $cpf = $filtro['cpf'];
+    //                 $query = ("SELECT * FROM usuario user WHERE 1=1 AND user.nr_cpf like '%$cpf%' ORDER BY user.nm_usuario ASC");
+    //                 $this->conexao = Conexao::connect()->query($query);
+    //                 $this->conexao->execute();
+    //                 $this->conexao = null;
+    //             }
+    //     }
+    // }
 }
--- visao/Filter.php
+++ visao/Filter.php
@@ -1,15 +1,35 @@
+<?php
+include_once DIR_PERSISTENCIA . 'UsuarioDAO.class.php';
+
+
+
+function listarComFiltro(){
+    $daoN = new UsuarioDAO();    
+    //return $daoN->listar("AND user.nm_usuario like %$filter[name]% AND user.nr_cpf like %$filter[cpf]%");
+    return $daoN->listar("AND u.nm_usuario like '%us%' AND u.nr_cpf like '%1%'");
+}
+
+
+
+
+$vetor = listarComFiltro();
+foreach ($vetor as $elemento) {
+    echo 'Usuario: ' . $elemento->getNmUsuario() . '<br>'.  'CPF: ' . $elemento->getNrCpf() . '<br>';
+}
+
+?>
+
 
 <div class='container-filter' >
 <form method="post">
 <h3>Filtro</h3>
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome">
-    <label for="nome">CPF:</label>
-    <input type="text" id="nome" name="nome">
+    <label for="name">Nome:</label>
+    <input type="text" id="name" name="name">
+    <label for="cpf">CPF:</label>
+    <input type="text" id="cpf" name="cpf">
+    <br>    
     <br>
-    <br>
-    <button type="submit" name="submit">Buscar</button>
+    <button class="filtrar" type="submit" name="submit">Buscar</button>
 </form>
 </div>
-
 
